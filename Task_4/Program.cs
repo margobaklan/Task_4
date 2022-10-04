071d9eeda0b1e17a3a527b520ef657b760f3220b
margobaklan <margo.baklan@gmail.com>
2022-10-04 18:51:04 +0300

    Добавьте файлы проекта.

---
+++ Task_4/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace Task_4
+{
+    abstract class GeoObject
+    {
+        public int CooX;
+        public int CooY;
+        public string Name;
+        public string Description;
+
+        public virtual void ShowInfo()
+        {
+            Console.WriteLine("Назва: {0}", Name);
+            Console.WriteLine("Координати: {0} , {1}", CooX, CooY);
+            Console.WriteLine("Опис: {0}", Description);
+        }
+    }
+
+    class River : GeoObject
+    {
+        public double Speed;
+        public double Length;
+        public override void ShowInfo()
+        {
+            base.ShowInfo();
+            Console.WriteLine("Швидкість: {0} м/с", Speed);
+            Console.WriteLine("Довжина: {0} км\n", Length);
+        }
+    }
+    class Mountain : GeoObject
+    {
+        public int HighestPoint;
+
+        public override void ShowInfo()
+        {
+            base.ShowInfo();
+            Console.WriteLine("Найвища точка: {0}\n", HighestPoint);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+            GeoObject dnipro = new River()
+            {
+                CooX = 0,
+                CooY = 0,
+                Name = "Дніпро",
+                Description = "четверта за довжиною і третя за площею басейну річка Європи",
+                Speed = 1.2,
+                Length = 2201
+            };
+            GeoObject Goverla = new Mountain()
+            {
+                CooX = 1,
+                CooY = 1,
+                Name = "Говерла",
+                Description = "найвища вершина Українських Карпат і найвища точка України",
+                HighestPoint = 2061
+            };
+
+            dnipro.ShowInfo();
+            Goverla.ShowInfo();
+        }
+    }
+}
---
+++ Task_4_2/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace Task_4_2
+{
+    interface GeoObject
+    {
+        int CooX { get; set; }
+        int CooY { get; set; }
+        string Name { get; set; }
+        string Description { get; set; }
+        void ShowInfo();
+        
+    }
+
+    class River : GeoObject
+    {
+        public int CooX { get; set; }
+        public int CooY { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Speed { get; set; }
+        public double Length { get; set; }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine("Назва: {0}", Name);
+            Console.WriteLine("Координати: {0} , {1}", CooX, CooY);
+            Console.WriteLine("Опис: {0}", Description);
+            Console.WriteLine("Швидкість: {0}", Speed);
+            Console.WriteLine("Довжина: {0}\n", Length);
+        }
+    }
+    class Mountain : GeoObject
+    {
+        public int CooX { get; set; }
+        public int CooY { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int HighestPoint { get; set; }
+        public void ShowInfo()
+        {
+            Console.WriteLine("Назва: {0}", Name);
+            Console.WriteLine("Координати: {0} , {1}", CooX, CooY);
+            Console.WriteLine("Опис: {0}", Description);
+            Console.WriteLine("Найвища точка: {0} м \n", HighestPoint);
+        }
+    }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode; 
+            
+            GeoObject dnipro = new River() 
+            {
+                CooX = 0,
+                CooY = 0,
+                Name = "Дніпро",
+                Description = "четверта за довжиною і третя за площею басейну річка Європи",
+                Speed = 1.2,
+                Length = 2201
+            };
+            GeoObject Goverla = new Mountain()
+            {
+                CooX = 1,
+                CooY = 1,
+                Name = "Говерла",
+                Description = "найвища вершина Українських Карпат і найвища точка України",
+                HighestPoint = 2061
+            };
+
+            dnipro.ShowInfo();
+            Goverla.ShowInfo();
+        }
+    }
+}
